9a83811e75a6bddd326ac785c6d224e814f60fd8
Gavin Mogan <git@gavinmogan.com>
2023-07-15 12:42:55 -0700

    add more tests

--- embedded_config/test/config_generator_test.dart
+++ embedded_config/test/config_generator_test.dart
@@ -8,33 +8,141 @@ import 'test_utils/test_utils.dart';
 
 void main() {
   group('ConfigGenerator', () {
-    test('yaml', () async {
-      await testGenerator(config: {
-        'test_yaml_config': {'source': ['lib/test_yaml_config.yaml']}
-      }, assets: {
-        'a|lib/test_yaml_config.yaml': '''
-            string: value
-        ''',
-        'a|lib/test.dart': '''
-          import 'package:embedded_config_annotations/embedded_config_annotations.dart';
+    group('yaml', () {
+      test('basic', () async {
+        await testGenerator(config: {
+          'test_config': {'source': ['lib/test_config.yaml']}
+        }, assets: {
+          'a|lib/test_config.yaml': '''
+              string: value
+          ''',
+          'a|lib/test.dart': '''
+            import 'package:embedded_config_annotations/embedded_config_annotations.dart';
 
-          part 'test.embedded.dart';
+            part 'test.embedded.dart';
 
-          @EmbeddedConfig('test_yaml_config')
-          abstract class TestYamlConfig {
-            const TestYamlConfig();
+            @EmbeddedConfig('test_config')
+            abstract class TestYamlConfig {
+              const TestYamlConfig();
 
-            String get string;
+              String get string;
+            }
+          '''
+        }, outputs: {
+          'a|lib/test.embedded.dart': (CompilationUnit unit) {
+            expect(unit.declarations, hasClass(r'_$TestYamlConfigEmbedded'));
+            final $class = getClass(unit, r'_$TestYamlConfigEmbedded')!;
+
+            testField($class, 'string', 'value');
           }
-        '''
-      }, outputs: {
-        'a|lib/test.embedded.dart': (CompilationUnit unit) {
-          expect(unit.declarations, hasClass(r'_$TestYamlConfigEmbedded'));
-          final $class = getClass(unit, r'_$TestYamlConfigEmbedded')!;
-
-          testField($class, 'string', 'value');
-        }
+        });
       });
+      test('generates correctly named and typed fields', () async {
+        await testGenerator(config: {
+          'test_config': {'source': ['lib/test_config.yaml']}
+        }, assets: {
+          'a|lib/test_config.yaml': '''
+              string: value
+              doubleWithInt: 0
+              numWithInt: 0
+              numWithDouble: 0.0
+              boolean: true
+              stringList:
+                  - a
+                  - b
+              intList: [0, 1]
+              doubleList: [0.4, 1.4]
+              doubleListWithInts: [0, 1, 2.4]
+              untypedList:
+                - a
+                - 0
+                - 1.4
+                - true
+                - null
+                - ['b', false, null]
+              stringMap: {'key': 'value'}
+              numMap:
+                  int: 0
+                  double: 0.0
+              boolMap:
+                  key: true
+              stringListMap:
+                key:
+                    - value
+              untypedMap:
+                string: value
+                num: 2.4
+                boolean: true
+                list:
+                  - a
+                  - 0
+                null: null
+          ''',
+          'a|lib/test.dart': r'''
+            import 'package:embedded_config_annotations/embedded_config_annotations.dart';
+
+            part 'test.embedded.dart';
+
+            @EmbeddedConfig('test_config')
+            abstract class TestConfig {
+              String get string;
+              double get doubleWithInt;
+              num get numWithInt;
+              num get numWithDouble;
+              bool get boolean;
+              List<String> get stringList;
+              List<int> get intList;
+              List<double> get doubleList;
+              List<double> get doubleListWithInts;
+              List get untypedList;
+              Map<String, String> get stringMap;
+              Map<String, num> get numMap;
+              Map<String, bool> get boolMap;
+              Map<String, List<String>> get stringListMap;
+              Map get untypedMap;
+              const TestConfig();
+            }
+          '''
+        }, outputs: {
+          'a|lib/test.embedded.dart': (CompilationUnit unit) {
+            expect(unit.declarations, hasClass(r'_$TestConfigEmbedded'));
+            final $class = getClass(unit, r'_$TestConfigEmbedded')!;
+
+            testField($class, 'string', 'value');
+            testField($class, 'doubleWithInt', 0);
+            testField($class, 'numWithInt', 0);
+            testField($class, 'numWithDouble', 0.0);
+            testField($class, 'boolean', true);
+            testField($class, 'stringList', ['a', 'b']);
+            testField($class, 'intList', [0, 1]);
+            testField($class, 'doubleList', [0.4, 1.4]);
+            testField($class, 'doubleListWithInts', [0, 1, 2.4]);
+            testField($class, 'untypedList', [
+              'a',
+              0,
+              1.4,
+              true,
+              null,
+              ['b', false, null]
+            ]);
+            testField($class, 'doubleListWithInts', [0, 1, 2.4]);
+            testField($class, 'stringMap', {'key': 'value'});
+            testField($class, 'numMap', {'int': 0, 'double': 0.0});
+            testField($class, 'boolMap', {'key': true});
+            testField($class, 'stringListMap', {
+              'key': ['value']
+            });
+            testField($class, 'untypedMap', {
+              'string': 'value',
+              'num': 2.4,
+              'boolean': true,
+              'list': ['a', 0],
+              'null': null
+            });
+          }
+        });
+      });
+
     });
     test('generates a correctly named class', () async {
       await testGenerator(config: {
